c7c86a05f5f16ec2a4dbb4de9eaa30bb2e4fdb77
Rafael Paes Leme <11760799+rafaelspaesleme-ads@users.noreply.github.com>
2019-05-07 15:37:32 -0300

    Update concatString.java

--- concatString.java
+++ concatString.java
@@ -1,5 +1,5 @@
 public class concatString {
-    public static String concatena(String tipo1, String tipo2) {
+    public static concatena(String tipo1, String tipo2) {
         return tipo1+tipo2;
   }
 }
